026c0c314ee75dddf867a910a378125fc8bddbff
proof.K <koftwro@gmail.com>
2021-04-03 23:03:45 +0800

    chore: 新增 deploy 需要的 lib 和 設定檔

---
+++ LineChatBot/deploy.js
@@ -0,0 +1,20 @@
+const code = require("zip-lib");
+
+
+
+if(process.argv.length != 3){
+
+    console.log("指令錯誤");
+
+
+    return;
+}
+
+
+/* 部署 line 的 webhook */
+
+if(process.argv[2] === "webhook"){
+
+    
+
+}
