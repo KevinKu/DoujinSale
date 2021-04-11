1424c449adb10203f59d6914af7d41ee0b97ffe9
proof.K <koftwro@gmail.com>
2021-04-11 23:17:46 +0800

    feat: api path 處理 line event

--- LineChatBot/WebHook/index.js
+++ LineChatBot/WebHook/index.js
@@ -1,10 +1,53 @@
 const express = require("express");
 const server = require("serverless-http");
+const bodyParser = require('body-parser');
+const line = require("@line/bot-sdk");
+
+
+
+const client = new line.Client({
+    channelAccessToken: process.env["channelAccessToken"] 
+  });
+
 
 
 const chatbot = express();
+
+/* chatbot server 設定 */
+
+chatbot.use(bodyParser.urlencoded({ extended: false }));
+
+chatbot.use(bodyParser.json())
+
+/* chatbot API */
+
+chatbot.post("/api", (req , res) => {
+
+    console.log(JSON.stringify(req["body"]));
+
+
+    res.send("");
+
+});
+
+
+
+
+
+
+
+
 
 
 
 
 const chatbot_server = server(chatbot);
+
+
+exports.handler = async (input , context) => {
+
+  let res = await chatbot_server(input);
+
+  return res;
+
+}
