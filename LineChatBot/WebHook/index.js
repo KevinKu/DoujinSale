bac4278f5c6dedb162f136c6e440306a810aa3a8
proof.K <koftwro@gmail.com>
2021-04-19 01:10:13 +0800

    feat: 點下選擇場次 , show 出 flex message

--- LineChatBot/WebHook/index.js
+++ LineChatBot/WebHook/index.js
@@ -5,8 +5,9 @@ const line = require("@line/bot-sdk");
 
 
 
-const client = new line.Client({
-    channelAccessToken: process.env["channelAccessToken"] 
+const line_bot = new line.Client({
+    channelAccessToken: process.env["channelAccessToken"],
+    channelSecret: process.env["channelSecret"]
   });
 
 
@@ -21,9 +22,35 @@ chatbot.use(bodyParser.json())
 
 /* chatbot API */
 
-chatbot.post("/api", (req , res) => {
+chatbot.post("/api",async (req , res) => {
 
     console.log(JSON.stringify(req["body"]));
+
+    let event_list = req["body"]["events"];
+
+
+    for(let i = 0; i < event_list.length; i++){
+
+      let event = event_list[i];
+
+      if(event["type"] === "postback"){
+
+          let postback_input = event["postback"]["data"];
+
+          let value = postback_input.split("=")[1];
+
+          /* 回應最近場次列表 */
+
+          if(value === "Session"){
+
+            await show_session_list(event["replyToken"]);
+
+          }
+
+
+      }
+
+    }
 
 
     res.send("");
@@ -51,3 +78,64 @@ exports.handler = async (input , context) => {
   return res;
 
 }
+
+
+
+const show_session_list = async (replyToken) => {
+
+      console.log(replyToken);
+
+      let session_list = {
+        type: "flex",
+        "altText": "this is a flex message",
+        contents: {
+          "type": "carousel",
+          "contents": [
+            {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "FF37",
+                    "action": {
+                      "type": "uri",
+                      "label": "action",
+                      "uri": "http://linecorp.com/"
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              "type": "bubble",
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "FF38",
+                    "action": {
+                      "type": "uri",
+                      "label": "action",
+                      "uri": "http://linecorp.com/"
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      };
+
+
+      
+
+
+      await line_bot.replyMessage(replyToken , session_list);
+
+      return;
+    }
