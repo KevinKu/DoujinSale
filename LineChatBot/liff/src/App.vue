d172dc7c9c2672181528bf9a58c94c01480e002c
proof.K <koftwro@gmail.com>
2021-06-15 20:46:12 +0800

    feat: 補上選攤位的畫面

--- LineChatBot/liff/src/App.vue
+++ LineChatBot/liff/src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="footer">
+      <van-swipe 
+        class="booth-number"
+        vertical
+      >
+        <van-swipe-item
+          v-for="number1 in booth_number_1"
+          :key="number1"
+          class="swipe-item"
+        >
+        {{ number1 }}
+        </van-swipe-item>
+      </van-swipe>
+      <van-swipe 
+        class="booth-number"
+        vertical
+      >
+        <van-swipe-item
+          v-for="number2 in booth_number_2"
+          :key="number2"
+          class="swipe-item"
+        >
+        {{ number2 }}
+        </van-swipe-item>
+      </van-swipe>
+    </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Swipe, SwipeItem } from "vant";
+
+
+
+
+const booth_number_1 = ["A","B","C"];
+
+
+const booth_number_2 = ["1","2","3"];
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem
+  },
+  data(){
+      return {
+        booth_number_1 : booth_number_1,
+        booth_number_2 : booth_number_2
+      };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+
+.footer {
+  height: 30%;
+  width: 100%;
+  position: fixed;
+  bottom: 0%;
+  z-index: 1;
+  display: flex;
+  justify-content: center; 
+  padding-bottom: 5vh;
 }
+
+.booth-number {
+  border: solid;
+  width: 40%;
+  display: block;
+  text-align: center;
+  font-size: 30vw;
+  overflow: hidden;
+}
+
 </style>
